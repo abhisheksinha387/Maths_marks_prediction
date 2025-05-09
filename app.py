2d87725819f4bb76f8f3ef0a9049717ab2cef5f9
Abhishek Sinha <abhisheksinha.sae.computer@gmail.com>
2025-05-09 15:30:23 +0530

    deployement

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-app = Flask(__name__)
+application=Flask(__name__)
 
+app=application
 
 ## Route for a home page
 
@@ -25,8 +26,8 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -41,6 +42,6 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(debug=True)        
+    app.run(host="0.0.0.0")        
 
 
